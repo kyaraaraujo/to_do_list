7bc14fe78583636bfb3b9d5ad8c8f6c4bf88fdc7
Kyara Araújo <araujomrk@gmail.com>
2022-05-26 16:25:26 -0300

    add help button and refactoring.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:to_do_list/show_dialog.dart';
+import 'package:to_do_list/show_alert_dialog.dart';
 
 import 'my_list.dart';
 
@@ -18,47 +18,56 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('My Task List')),
-        body: Column(children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(children: [
-              Expanded(
-                child: TextField(controller: controller),
-              ),
-              IconButton(
-                  icon: const Icon(Icons.add),
-                  onPressed: () {
-                    final textInput = controller.text.trim();
-                    setState(() {
-                      if (textInput.isNotEmpty) {
-                        getMyList().add(textInput);
-                      }
-                    });
-                    controller.clear();
-                  })
-            ]),
-          ),
-          Expanded(
-              child: ListView.builder(
-                  itemCount: getMyList().length,
-                  itemBuilder: (context, index) {
-                    final item = getMyList()[index];
+      appBar: AppBar(title: const Text('My Task List')),
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(children: [
+            Expanded(
+              child: TextField(controller: controller),
+            ),
+            IconButton(
+                icon: const Icon(Icons.add),
+                onPressed: () {
+                  final textInput = controller.text.trim();
+                  setState(() {
+                    if (textInput.isNotEmpty) {
+                      getMyList().add(textInput);
+                    }
+                  });
+                  controller.clear();
+                })
+          ]),
+        ),
+        Expanded(
+            child: ListView.builder(
+                itemCount: getMyList().length,
+                itemBuilder: (context, index) {
+                  final item = getMyList()[index];
 
-                    return ListTile(
-                        title: Text(item),
-                        onLongPress: () async {
-                          final action = await DialogAction.dialogOptions(
-                              context,
-                              'Delete Action',
-                              'Do you want to remove it?',
-                              'Yes',
-                              'No');
-                          if (action == DialogOptionsEnum.confirm) {
-                            setState(() => getMyList().remove(item));
-                          }
-                        });
-                  })),
-        ]));
+                  return ListTile(
+                      title: Text(item),
+                      onLongPress: () async {
+                        final action = await ShowAlertDialog.dialogOptions(
+                            context,
+                            'Delete Action',
+                            'Do you want to remove it?',
+                            'Yes',
+                            'No');
+                        if (action == DialogOptionsEnum.confirm) {
+                          setState(() => getMyList().remove(item));
+                        }
+                      });
+                })),
+      ]),
+      floatingActionButton: FloatingActionButton.small(
+        onPressed: () async {
+          await ShowAlertDialog.dialogOptions(context, 'Help',
+              'Long press in the task to remove it.', 'Ok', '');
+        },
+        child: const Icon(Icons.help),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+    );
   }
 }
--- lib/show_alert_dialog.dart
+++ lib/show_alert_dialog.dart
@@ -2,7 +2,23 @@ import 'package:flutter/material.dart';
 
 enum DialogOptionsEnum { confirm, cancel }
 
-class DialogAction {
+class ShowAlertDialog {
+  static Widget _addCancelButtonIfRequired(
+      BuildContext context, String cancelOption) {
+    return TextButton(
+        child: Text(cancelOption, style: const TextStyle(color: Colors.red)),
+        onPressed: () => Navigator.of(context).pop(DialogOptionsEnum.cancel));
+  }
+
+  static Widget _confirmButton(BuildContext context, String confirmOption) {
+    return TextButton(
+        child: Text(
+          confirmOption,
+          style: const TextStyle(color: Colors.black),
+        ),
+        onPressed: () => Navigator.of(context).pop(DialogOptionsEnum.confirm));
+  }
+
   static Future<DialogOptionsEnum> dialogOptions(
       BuildContext context,
       String dialogTitle,
@@ -23,18 +39,9 @@ class DialogAction {
             ),
           ),
           actions: <Widget>[
-            TextButton(
-                child: Text(
-                  confirmOption,
-                  style: const TextStyle(color: Colors.black),
-                ),
-                onPressed: () =>
-                    Navigator.of(context).pop(DialogOptionsEnum.confirm)),
-            TextButton(
-                child: Text(cancelOption,
-                    style: const TextStyle(color: Colors.red)),
-                onPressed: () =>
-                    Navigator.of(context).pop(DialogOptionsEnum.cancel)),
+            _confirmButton(context, confirmOption),
+            if (cancelOption.isNotEmpty)
+              _addCancelButtonIfRequired(context, cancelOption)
           ],
         );
       },
